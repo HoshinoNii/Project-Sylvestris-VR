75cd5aef239769d86b1ff1bcb90021601fa3245a
Kiseki_HT <74333787+KisekiNii@users.noreply.github.com>
2022-01-19 11:13:06 +0800

    Added in a enum converter

--- Pinus Sylvestris/Assets/Scripts/Core/Utils/Utils.cs
+++ Pinus Sylvestris/Assets/Scripts/Core/Utils/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Sylvestris.Core.Utils {
@@ -11,6 +12,30 @@ namespace Sylvestris.Core.Utils {
 					material.color.a - Time.deltaTime / time);
 				yield return null;
 			}
+		}
+	}
+	
+	public static class EnumHelper
+	{
+		public static  T GetEnumValue<T>(string str) where T : struct, IConvertible
+		{
+			Type enumType = typeof(T);
+			if (!enumType.IsEnum)
+			{
+				throw new Exception("T must be an Enumeration type.");
+			}
+			return Enum.TryParse(str, true, out T val) ? val : default;
+		}
+
+		public static T GetEnumValue<T>(int intValue) where T : struct, IConvertible
+		{
+			Type enumType = typeof(T);
+			if (!enumType.IsEnum)
+			{
+				throw new Exception("T must be an Enumeration type.");
+			}
+        
+			return (T)Enum.ToObject(enumType, intValue);
 		}
 	}
 }
