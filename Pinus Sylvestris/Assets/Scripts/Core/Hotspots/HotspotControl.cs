4442c2ab8ac8aa574fe9ca8cdb1b0a6cc369f0d7
Kiseki_HT <74333787+KisekiNii@users.noreply.github.com>
2022-01-19 10:18:41 +0800

    Hover

--- Pinus Sylvestris/Assets/Scripts/Core/Hotspots/HotspotControl.cs
+++ Pinus Sylvestris/Assets/Scripts/Core/Hotspots/HotspotControl.cs
@@ -1,17 +1,43 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 namespace Sylvestris.Core.Inputs
 {
-    public class HotspotControl : MonoBehaviour
-    {
-        public void OnEnable()
-        {
-            gameObject.SetActive(true);
+    public class HotspotControl : MonoBehaviour {
+        private Animator Animator => GetComponent<Animator>();
+        private int FadeInTrigger => Animator.StringToHash("FadeIn");
+        private int FadeOutTrigger => Animator.StringToHash("FadeOut");
+
+
+        private void OnEnable() {
+            StartCoroutine(Fade(false)); //FADE OUT
         }
 
-        public void OnDisable()
-        {
-            gameObject.SetActive(false);
+
+        public void FadeIn() {
+            StartCoroutine(Fade(true));
         }
+
+        public void FadeOut()
+        {
+            StartCoroutine(Fade(false));
+        }
+
+        private IEnumerator Fade(bool fadeIn) {
+            if (fadeIn) {
+                Animator.SetTrigger(FadeInTrigger);
+                Animator.ResetTrigger(FadeOutTrigger);
+            } else {
+                Animator.SetTrigger(FadeOutTrigger);
+                Animator.ResetTrigger(FadeInTrigger);
+                yield return new WaitForSeconds(.3f);
+            }
+        }
+        
+        private void SetState(bool state)
+        {
+            gameObject.SetActive(state);
+        } 
     }
 }
